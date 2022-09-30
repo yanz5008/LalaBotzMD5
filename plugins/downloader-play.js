e2798a1066738ad38f5fe48c424b535ffa7cbfe4
yanz5008 <88087994+yanz5008@users.noreply.github.com>
2022-09-30 20:38:23 +0700

    downloader-play.js

--- plugins/downloader-play.js
+++ plugins/downloader-play.js
@@ -1,27 +1,29 @@
+import fetch from 'node-fetch'
 import { youtubeSearch } from '@bochilteam/scraper'
-let handler = async (m, { conn, command, text, usedPrefix }) => {
-  if (!text) throw '\nApa yg mau dicari kak?\n\nContoh: .play mengharapkanmu'
+
+let handler = async (m, { conn, text, usedPrefix }) => {
+  if (!text) throw 'Input Query'
   let vid = (await youtubeSearch(text)).video[0]
-  if (!vid) throw 'Video/Audio Tidak ditemukan'
-  let { title, description, thumbnail, videoId, durationH, viewH, publishedTime } = vid
-  const url = 'https://www.youtube.com/watch?v=' + videoId
-  await conn.sendButton(m.chat, `
-• YOUTUBE PLAY •
-
-${htjava} Title: ${title}
-${htjava} Durasi: ${durationH}
-
-
-${htjava} Url: ${url}`, botdate, thumbnail, [
-    ['🔎 Youtube Search', `${usedPrefix}yts ${url}`],
-   ['🎶 Audio', `${usedPrefix}yta ${url} yes`],
-    ['🎥 Video', `${usedPrefix}ytv ${url} yes`]
-  ], m) 
+  if (!vid) throw 'Video/Audio Tidak Ditemukan'
+  let { title, description, thumbnail, videoId, durationH, durationS, viewH, publishedTime } = vid
+  let url = 'https://www.youtube.com/watch?v=' + videoId
+  let ytLink = `https://yt-downloader.akkun3704.repl.co/?url=${url}&filter=audioonly&quality=highestaudio&contenttype=audio/mpeg`
+  let capt = `*Title:* ${title}\n*Published:* ${publishedTime}\n*Duration:* ${durationH}\n*Views:* ${viewH}\n*Url:* ${url}`
+  // let buttons = [{ buttonText: { displayText: 'Video' }, buttonId: `${usedPrefix}ytv ${url}` }]
+  let msg = await conn.sendButton(m.chat, capt, '_Audio on progress..._', thumbnail, ['Video',  `${usedPrefix}ytv ${url}`], m, { viewOnce: true }) 
+ // if (durationS > 4000) return conn.sendMessage(m.chat, { text: `*Download:* ${await shortUrl(ytLink)}\n\n_Duration too long..._` }, { quoted: msg })
+  conn.sendMessage(m.chat, { audio: { url: ytLink }, mimetype: 'audio/mpeg' }, { quoted: msg })
 }
-handler.help = ['play']
+handler.help = handler.alias = ['play']
 handler.tags = ['downloader']
-handler.command = /^play2?$/i
-
-handler.register = true
+handler.command = /^(play)$/i
+handler.exp = 0
 
 export default handler
+
+async function shortUrl(url) {
+  url = encodeURIComponent(url)
+  let res = await fetch(`https://is.gd/create.php?format=simple&url=${url}`)
+  if (!res.ok) throw false
+  return await res.text()
+}
